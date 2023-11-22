df72ed4acb58c4a1a9c86e6d19853b0330751726
rayahiqbal <146009700+rayahiqbal@users.noreply.github.com>
2023-11-22 16:14:41 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,99 +1,111 @@
-const textElement = document.getElementById('text')
-const optionButtonsElement = document.getElementById('option-buttons')
+const textElement = document.getElementById('text');
+const optionButtonsElement = document.getElementById('option-buttons');
 
-let state = {}
+const GameState = {
+  PURPLE_CRYSTAL: 'purpleCrystal',
+  SWORD: 'sword',
+  SHIELD: 'shield',
+};
+
+let state = {};
 
 function startGame() {
-  state = {}
-  showTextNode(1)
+  state = {};
+  showTextNode(1);
 }
 
 function showTextNode(textNodeIndex) {
-  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
-  textElement.innerText = textNode.text
+  const textNode = textNodes.find((node) => node.id === textNodeIndex);
+  textElement.innerText = textNode.text;
+
   while (optionButtonsElement.firstChild) {
-    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild);
   }
 
-  textNode.options.forEach(option => {
+  textNode.options.forEach((option) => {
     if (showOption(option)) {
-      const button = document.createElement('button')
-      button.innerText = option.text
-      button.classList.add('btn')
-      button.addEventListener('click', () => selectOption(option))
-      optionButtonsElement.appendChild(button)
+      const button = createOptionButton(option);
+      optionButtonsElement.appendChild(button);
     }
-  })
+  });
 }
 
 function showOption(option) {
-  return option.requiredState == null || option.requiredState(state)
+  return option.requiredState == null || option.requiredState(state);
 }
 
 function selectOption(option) {
-  const nextTextNodeId = option.nextText
+  const nextTextNodeId = option.nextText;
   if (nextTextNodeId <= 0) {
-    return startGame()
+    return startGame();
   }
-  state = Object.assign(state, option.setState)
-  showTextNode(nextTextNodeId)
+  state = { ...state, ...option.setState };
+  showTextNode(nextTextNodeId);
+}
+
+function createOptionButton(option) {
+  const button = document.createElement('button');
+  button.innerText = option.text;
+  button.classList.add('btn');
+  button.addEventListener('click', () => selectOption(option));
+  return button;
 }
 
 const textNodes = [
   {
     id: 1,
-    text: 'You wake up in a strange place and you see a jar of green goo near you.',
+    text: 'You wake up in a strange place and you see a jar of purple crystal near you.',
     options: [
       {
-        text: 'Take the goo',
-        setState: { greenGoo: true },
-        nextText: 2
+        text: 'Take the crystal',
+        setState: { purpleCrystal: true },
+        nextText: 2,
       },
       {
-        text: 'Leave the goo',
-        nextText: 2
-      }
-    ]
+        text: 'Leave the crystal',
+        nextText: 2,
+      },
+    ],
   },
   {
     id: 2,
     text: 'You venture forth in search of answers to where you are when you come across a merchant.',
     options: [
       {
-        text: 'Trade the goo for a sword',
-        requiredState: (currentState) => currentState.greenGoo,
-        setState: { greenGoo: false, sword: true },
-        nextText: 3
+        text: 'Trade the crystal for a sword',
+        requiredState: (currentState) => currentState.purpleCrystal,
+        setState: { purpleCrystal: false, sword: true },
+        nextText: 3,
       },
       {
-        text: 'Trade the goo for a shield',
-        requiredState: (currentState) => currentState.greenGoo,
-        setState: { greenGoo: false, shield: true },
-        nextText: 3
+        text: 'Trade the crystal for a shield',
+        requiredState: (currentState) => currentState.purpleCrystal,
+        setState: { purpleCrystal: false, shield: true },
+        nextText: 3,
       },
       {
         text: 'Ignore the merchant',
-        nextText: 3
-      }
-    ]
+        nextText: 3,
+      },
+    ],
   },
   {
     id: 3,
-    text: 'After leaving the merchant you start to feel tired and stumble upon a small town next to a dangerous looking castle.',
+    text: 'After leaving the merchant, you start to feel tired and stumble upon a small town next to a dangerous-looking castle.',
     options: [
       {
         text: 'Explore the castle',
-        nextText: 4
+        nextText: 4,
       },
       {
         text: 'Find a room to sleep at in the town',
-        nextText: 5
+        nextText: 5,
       },
       {
         text: 'Find some hay in a stable to sleep in',
-        nextText: 6
-      }
-    ]
+        nextText: 6,
+      },
+    ],
   },
   {
     id: 4,
@@ -101,54 +113,54 @@ const textNodes = [
     options: [
       {
         text: 'Restart',
-        nextText: -1
-      }
-    ]
+        nextText: -1,
+      },
+    ],
   },
   {
     id: 5,
-    text: 'Without any money to buy a room you break into the nearest inn and fall asleep. After a few hours of sleep the owner of the inn finds you and has the town guard lock you in a cell.',
+    text: 'Without any money to buy a room, you break into the nearest inn and fall asleep. After a few hours of sleep, the owner of the inn finds you and has the town guard lock you in a cell.',
     options: [
       {
         text: 'Restart',
-        nextText: -1
-      }
-    ]
+        nextText: -1,
+      },
+    ],
   },
   {
     id: 6,
-    text: 'You wake up well rested and full of energy ready to explore the nearby castle.',
+    text: 'You wake up well-rested and full of energy ready to explore the nearby castle.',
     options: [
       {
         text: 'Explore the castle',
-        nextText: 7
-      }
-    ]
+        nextText: 7,
+      },
+    ],
   },
   {
     id: 7,
-    text: 'While exploring the castle you come across a terrible monster in your path.',
+    text: 'While exploring the castle, you come across a terrible monster in your path.',
     options: [
       {
         text: 'Try to run',
-        nextText: 8
+        nextText: 8,
       },
       {
         text: 'Attack it with your sword',
         requiredState: (currentState) => currentState.sword,
-        nextText: 9
+        nextText: 9,
       },
       {
         text: 'Hide behind your shield',
         requiredState: (currentState) => currentState.shield,
-        nextText: 10
+        nextText: 10,
       },
       {
-        text: 'Throw the goo at it',
-        requiredState: (currentState) => currentState.blueGoo,
-        nextText: 11
-      }
-    ]
+        text: 'Throw the crystal at it',
+        requiredState: (currentState) => currentState.purpleCrystal,
+        nextText: 11,
+      },
+    ],
   },
   {
     id: 8,
@@ -156,9 +168,9 @@ const textNodes = [
     options: [
       {
         text: 'Restart',
-        nextText: -1
-      }
-    ]
+        nextText: -1,
+      },
+    ],
   },
   {
     id: 9,
@@ -166,9 +178,9 @@ const textNodes = [
     options: [
       {
         text: 'Restart',
-        nextText: -1
-      }
-    ]
+        nextText: -1,
+      },
+    ],
   },
   {
     id: 10,
@@ -176,20 +188,20 @@ const textNodes = [
     options: [
       {
         text: 'Restart',
-        nextText: -1
-      }
-    ]
+        nextText: -1,
+      },
+    ],
   },
   {
     id: 11,
-    text: 'You threw your jar of goo at the monster and it exploded. After the dust settled you saw the monster was destroyed. Seeing your victory you decide to claim this castle as your own and live out the rest of your days there.',
+    text: 'You threw your jar of crystal at the monster and it exploded. After the dust settled you saw the monster was destroyed. Seeing your victory you decide to claim this castle as your own and live out the rest of your days there.',
     options: [
       {
         text: 'Congratulations. Play Again.',
-        nextText: -1
-      }
-    ]
-  }
-]
+        nextText: -1,
+      },
+    ],
+  },
+];
 
-startGame()
+startGame();
